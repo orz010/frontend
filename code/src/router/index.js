3c453087f4f612fa173afe05ce82068c00878f2e
orz010 <3320642802@qq.com>
2022-05-15 15:02:41 +0800

    vue3-1vue3-1vue3-1vue3-1vue3-1vue3-1vue3-1vue3-1vue3-1vue3-1vue3-1

--- code/src/router/index.js
+++ code/src/router/index.js
@@ -14,6 +14,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/discussion',
+    name: 'discussion',
+    component: () => import(/* webpackChunkName: "about" */ '../views/DiscussionView.vue')
   }
 ]
 
